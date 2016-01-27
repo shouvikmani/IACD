c709cdeb4d4410454fcb8535f925e150ad8c7b40
Shoubik Mani <shouvikm@andrew.cmu.edu>
2016-01-27 17:55:41 -0500

    Switch from mapbox.js to leaflet.js

--- Map_Visualizations/drone_map_vis.js
+++ Map_Visualizations/drone_map_vis.js
@@ -1,8 +1,10 @@
-console.log("HI");
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2hvdXZpa21hbmkiLCJhIjoiY2lqdmx6a3VhMDh0Y3VjbTVycXV2MW0xNiJ9.4MgtlH94XYLTO3TPEtqIbw';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-    center: [53.61, 17.64], // [longitude, latitude] order
-    zoom: 3 // starting zoom
-});
+//Initialize Leaflet map and styles
+var map = L.map('map').setView([17.64, 53.61], 4);
+var CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
+
+//Hides bottom label
+document.getElementsByClassName('leaflet-control-attribution leaflet-control')[0].style.display = 'none';
