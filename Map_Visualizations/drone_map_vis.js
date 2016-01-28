30e67027dd5cf5002782b2a86d699e5b1be349fa
Shoubik Mani <shouvikm@andrew.cmu.edu>
2016-01-27 19:03:06 -0500

    Get drone strike data, add colored markers

--- Map_Visualizations/drone_map_vis.js
+++ Map_Visualizations/drone_map_vis.js
@@ -6,5 +6,66 @@ var CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/
 	maxZoom: 19
 }).addTo(map);
 
+$.ajax({
+	type: 'GET',
+    url: 'http://api.dronestre.am/data',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+       console.log(json);
+       strikeData = json['strike'];
+       plotMarkers(strikeData);
+    },
+    error: function(e) {
+       console.log(e.message);
+    }
+})
+
+function plotMarkers(data) {
+	var marker;
+	for (var i = 0; i < data.length; i++) {
+		var strikeLat = data[i]['lat'];
+		var strikeLon = data[i]['lon'];
+		var region = data[i]['location'];
+		var country = data[i]['country'];
+		var date = getLocalDate(data[i]['date']);
+		var narrative = data[i]['narrative'];
+		var deaths = data[i]['deaths'];
+		var injuries = data[i]['injuries'];
+
+		marker = L.marker([strikeLat, strikeLon], {icon: getMarkerIcon(country)}).addTo(map);
+		marker.bindPopup("<b>Location: </b>" + region + ", " + country
+						+ "<br><b>Date: </b>" + date
+						+ "<br>" + narrative
+						+ "<br><br>"
+						+ "<b>Deaths: </b>" + deaths
+						+ "&nbsp;&nbsp;|&nbsp;&nbsp;"
+						+ "<b>Injuries: </b>" + injuries);
+	}
+}
+
+//Converts an ISO date time value to a local date time string 
+function getLocalDate(isoDateTime) {
+	var utcDate = new Date(isoDateTime);
+	var localDate = utcDate.toLocaleString();
+
+	return localDate;
+}
+
+function getMarkerIcon(country) {
+	if (country === 'Somalia') {
+		return L.icon({iconUrl: 'static/blue_marker.svg'});
+	} else if (country === 'Yemen') {
+		return L.icon({iconUrl: 'static/red_marker.svg'});
+	} else if (country == 'Pakistan' || country === 'Pakistan-Afghanistan Border') {
+		return L.icon({iconUrl: 'static/green_marker.svg'});
+	} else {
+		return L.icon({iconUrl: 'static/white_marker.svg'});
+	}
+}
+
+
+
+
 //Hides bottom label
 document.getElementsByClassName('leaflet-control-attribution leaflet-control')[0].style.display = 'none';
