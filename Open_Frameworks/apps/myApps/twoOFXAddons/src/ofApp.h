91af0ba272456541063bb6d8af097527f4296d4d
Shoubik Mani <shouvikm@andrew.cmu.edu>
2016-01-28 07:57:06 -0500

    Compile 2 OpenFrameworkds addons together

---
+++ Open_Frameworks/apps/myApps/twoOFXAddons/src/ofApp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxScreenGrab.h"
+#include "ofxDatGui.h"
+
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    
+    int numClicks;
+    bool isFullscreen;
+    ofxDatGuiButton* button;
+    ofxDatGuiButton* toggle;
+    void positionButtons();
+    void onButtonEvent(ofxDatGuiButtonEvent e);
+    
+    ofxScreenGrab grabber;
+    
+};
