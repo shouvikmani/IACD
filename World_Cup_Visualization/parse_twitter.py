c00f13f713ccf6918840f8240c40628a02d4aad4
Shoubik Mani <shouvikm@andrew.cmu.edu>
2016-02-25 21:39:35 -0500

    Make multiple requests, write tweets to file, efficiency improvements

--- World_Cup_Visualization/parse_twitter.py
+++ World_Cup_Visualization/parse_twitter.py
@@ -4,11 +4,12 @@ import requests
 import urllib
 import json
 import datetime
+import sys
 from pprint import pprint
 from time import time
 
 from twitter_auth import authenticate
-from read_data import parseTweetIdCSV
+from read_data import readFile, writeToFile, setHeaders
 
 #Token global variable
 bearerToken = authenticate()
@@ -57,7 +58,7 @@ def getFollowerCount(screenName):
 	followerCount = getRequest(query_url)['followers_count']
 	return followerCount
 
-def getTweets(tweetIdList):
+def getTweetsFromId(tweetIdList):
 	base_url = 'https://api.twitter.com/1.1/statuses/lookup.json?'
 	idList = [tweet['id'] for tweet in tweetIdList]
 	idListString = ','.join(idList)
@@ -67,14 +68,57 @@ def getTweets(tweetIdList):
 	tweets = getRequest(query_url)
 	#Filters to only geotagged tweets
 	geoTweets = filter(lambda x: str(x['coordinates']) != 'None', tweets)
-	return geoTweets
+	reducedTweets = [
+		{
+			'id_str': tweet['id_str'],
+			'created_at': tweet['created_at'],
+			'coordinates': tweet['coordinates'],
+			'hashtags': tweet['entities']['hashtags'],
+			'text': tweet['text'].encode('ascii', 'ignore')
+		} for tweet in geoTweets
+	]
+	return reducedTweets
 
-def main():
-	#start = time()
-	tweetData = parseTweetIdCSV('data/tweets.csv')
-	#print "Parsing time: " + str(time() - start)
-	pprint(getTweets(tweetData))
-	#print "API Request time: " + str(time() - start)
+#Parses and returns a list of tweetIDs from a list of data
+#starting at startRow and ending at endRow
+def parseTweetIds(dataLines, startRow, endRow):
+	start = time()
+	dataList = []
+	for i in xrange(startRow, endRow+1):
+		instanceDict = dict()
+		instance = dataLines[i].split()
+		#Removing " because id formatted as '"id"' in tweets.csv
+		instanceDict['id'] = instance[0].replace('"', '')
+		dataList.append(instanceDict)
+	return dataList
+
+#TODO: Use end to enforce upper limit on requestEndRow
+def downloadAllTweets(start, end):
+	requestStartRow = start
+	requestEndRow = start + 99
+	numRequests = 0
+	tweetIdSource = 'data/tweets.csv'
+	tweetTargetSource = 'data/completeTweets.csv'
+	dataLines = readFile(tweetIdSource).splitlines()
+	fieldnames = ['id_str', 'created_at', 'coordinates', 'hashtags', 'text']
+	setHeaders(tweetIdSource, fieldnames)
+	while (numRequests < 60):
+		tweetIdList = parseTweetIds(dataLines, requestStartRow, requestEndRow)
+		tweetData = getTweetsFromId(tweetIdList)
+		print tweetData
+		print numRequests
+		print
+		writeToFile(tweetData, tweetTargetSource, fieldnames)
+		requestStartRow += 100
+		requestEndRow += 100
+		numRequests += 1
 	print getRateLimit()
 
-main()
+def main():
+	start = time()
+	startRow = int(sys.argv[1])
+	endRow = int(sys.argv[2])
+	downloadAllTweets(startRow, endRow)
+
+if __name__ == '__main__':
+	main()
